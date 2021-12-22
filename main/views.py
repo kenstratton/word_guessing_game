61c7e350d92a5644be72e9a04d772cf36dcedc2a
kenstratton <ken.stratton.2643@mail.linbenton.edu>
2021-12-22 21:34:30 +0900

    hj

--- main/views.py
+++ main/views.py
@@ -1,23 +1,31 @@
 # Import Flask and functions(allows app to show html)
-from flask import Flask, render_template, url_for
+from flask import render_template, url_for, redirect
 from main import app
 from main.models import Word
 from sqlalchemy import func
 import string
 
 
-# When accessed '/', shows the top page
+# When accessed, shows the top page
 @app.route('/')
 def top():
     return render_template(
-        'top.html', type="top")
+        'top.html', type='top')
 
-# When accessed '/ufo-coming', transfers to game
+# When accessed, transfers to game
 @app.route('/ufo-coming')
 def game():
     alphabets = list(string.ascii_uppercase)
     word = Word.query.order_by(func.random()).first()
-    secret_word = word.spelling.upper()
-    hint = word.category.name
-    return render_template(
-        'game.html', type="game", alph=alphabets, sec_word=secret_word, hint=hint)
+    if word:
+        secret_word = word.spelling.upper()
+        hint = word.category.name
+        return render_template(
+            'game.html', type='game', alph=alphabets, sec_word=secret_word, hint=hint)
+    else:
+        return redirect(url_for('top'))
+
+# When accessed, transfers to the administration page
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', type='admin')
